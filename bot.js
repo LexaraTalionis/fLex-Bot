299cb7ddc2afce2a5e6c9e2b7a8786a5c6fb3851
LexaraTalionis <53713091+LexaraTalionis@users.noreply.github.com>
2020-03-18 02:58:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const http = require('http');
+const https = require('https');
 const client = new Discord.Client();
 
 const wow = "https://worldofwarcraft.com/en-us/character/us/";
@@ -15,7 +15,7 @@ client.on('message', message => {
 	let realmDash = realm.replace(" ", "-");
 	let url = wow+realmDash+"/"+name;
 	
-	http.get(url, (result) => {
+	https.get(url, (result) => {
 		if (result.statusCode !== 200) {
 			message.channel.send("I can't seem to find "+name+" on "+realm+".");
 		} else {
