b43023f1def914ac7c4ffb275e8f3cd2559e1093
LexaraTalionis <53713091+LexaraTalionis@users.noreply.github.com>
2020-03-18 01:22:32 -0500

    Initial
    Initial upload.

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const wow = "https://worldofwarcraft.com/en-us/character/us/";
+
+client.on('ready', () => {
+    console.log("Logged in as ${client.user.tag}!");
+});
+
+client.on('message', message => {
+	var info = message.content.split("-");
+	var name = info[0];
+	var realm = info[1];
+	var realmDash = realm.replace(" ", "-");
+	var url = wow+realmDash+"/"+name;
+	
+	var fail = false;
+	var request = new XMLHttpRequest();
+	request.open("GET", url, true);
+	request.send();
+	if (request.status === 404) {
+		fail = true;
+	}
+	
+    if (fail) {
+		message.channel.send("I can't seem to find "+name+" on "+realm+".");
+    } else {
+		message.channel.send("Is this your character?\n"+url);
+	}
+});
+
+client.login("Njg5NjkxNjM2ODA4NDE3Mjgy.XnGn4w.Z7OSUvLpnR7xIG_mN-566w_LZrc");
