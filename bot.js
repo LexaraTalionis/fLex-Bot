65e21bfddbd28fd83283282f83d85a39b71b167b
LexaraTalionis <53713091+LexaraTalionis@users.noreply.github.com>
2020-03-18 02:55:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const http = require('http');
 const client = new Discord.Client();
 
 const wow = "https://worldofwarcraft.com/en-us/character/us/";
@@ -8,24 +9,19 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	var info = message.content.split("-");
-	var name = info[0];
-	var realm = info[1];
-	var realmDash = realm.replace(" ", "-");
-	var url = wow+realmDash+"/"+name;
+	let info = message.content.split("-");
+	let name = info[0];
+	let realm = info[1];
+	let realmDash = realm.replace(" ", "-");
+	let url = wow+realmDash+"/"+name;
 	
-	var request = new ActiveXObject("Microsoft.XMLHTTP");
-	request.open('GET', url, false);
-	request.onreadystatechange = function() {
-    if (request.readyState === 4){
-			if (request.status === 404) {  
-				message.channel.send("I can't seem to find "+name+" on "+realm+".");
-			} else {
-				message.channel.send("Is this your character?\n"+url);
-			}
+	http.get(url, (result) => {
+		if (result.statusCode !== 200) {
+			message.channel.send("I can't seem to find "+name+" on "+realm+".");
+		} else {
+			message.channel.send("Is this your character?\n"+url);
 		}
-	};
-	request.send();
+	});
 });
 
 client.login(process.env.BOT_TOKEN);
