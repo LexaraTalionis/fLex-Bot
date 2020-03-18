4fadaa44a678d6fb82fe43326b909bcfaa6ad704
LexaraTalionis <53713091+LexaraTalionis@users.noreply.github.com>
2020-03-18 04:19:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,63 @@
-const Discord = require('discord.js');
-const https = require('https');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const https = require("https");
 
 const wow = "https://worldofwarcraft.com/en-us/character/us/";
-
-client.on('ready', () => {
-    console.log("Logged in as "+client.user.tag+"!");
-});
-
-client.on('message', message => {
-	if (message.author.bot) {
-		return;
-	}
-	
-	var info = message.content.split("-");
+function parseName(str) {
+	var info = str.split("-");
 	var name = info[0];
-	console.log("Name: "+name);
 	var realm = info[1];
-	console.log("Realm: "+realm);
 	var realmDash = realm.replace(" ", "-");
 	var url = wow+realmDash+"/"+name;
-	console.log("URL: "+url);
 	
 	https.get(url, (result) => {
 		if (result.statusCode !== 200) {
-			message.channel.send("I can't seem to find "+name+" on "+realm+".");
+			return name, realm;
 		} else {
-			message.channel.send("Is this your character?\n"+url);
+			return name, realm, url;
 		}
 	});
+}
+
+const newUsers = {};
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("Logged in as "+client.user.tag+"!");
+});
+
+client.on("guildMemberAdd", user => {
+	newUsers[user.id] = {};
+	user.send("Welcome to the Lex Raid Discord! To access our Discord, please provide your character name and realm. IE: Lexara-Wyrmrest Accord");
+});
+
+client.on("message", message => {
+	if (message.channel.type == "dm") {
+		var info = newUsers[message.author.id];
+		
+		if (name in info && realm in info) {
+			if (message.content.startsWith("Yes")) {
+				
+			} else if (message.content.startsWith("No")) {
+				delete info.name;
+				delete info.realm;
+				
+				message.author.send("Let's try again. What is your character name and realm? IE: Lexara-Wyrmrest Accord");
+			} else {
+				message.author.send("I'm sorry, I don't understand. Is that your character? Type Yes or No.");
+			}
+		} else {
+			var name, realm, url = parseName(message.content);
+			
+			if (typeof url === "string") {
+				info.name = name;
+				info.realm = realm;
+				message.author.send(url+"\nIs this your character? Type Yes or No.");
+			} else {
+				message.author.send("I can't seem to find "+name+" on "+realm+". Let's try again. What is your character name and realm? IE: Lexara-Wyrmrest Accord");
+			}
+		}
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
